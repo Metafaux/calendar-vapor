22e1486d1c4cd5644a3df150e26b2c57676db7ca
Alex W <alex@metafaux.com>
2020-10-18 10:43:51 -0700

    Update placeholder; new month/day components

---
+++ src/MonthView/components/dayCell.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface DayCellProps {
+  monthIndex: number;
+}
+
+const DayCell = (props: DayCellProps) => {
+  return <div />;
+};
+
+export default DayCell;
---
+++ src/MonthView/monthView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface MonthViewProps {
+  monthIndex: number;
+}
+
+const MonthView = (props: MonthViewProps) => {
+  return <div />;
+};
+
+export default MonthView;
